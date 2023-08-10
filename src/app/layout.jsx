1f6ac6f7eba43f6ec32152911601393910e477a2
jola-blvhk <awujoolabello@gmail.com>
2023-08-10 23:27:32 +0100

    hot jar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
       <body className={`${comfortaa.className}`}>
         <Header />
         {children}
-      \
+
         <Footer />
       </body>
     </html>
