714ab290669c7b8c7fde69c947f338492fd78d99
jola-blvhk <awujoolabello@gmail.com>
2023-08-10 22:50:56 +0100

    hot jar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,14 +27,14 @@ export default function Home() {
 
   useEffect(() => {
     // Replace with your Hotjar tracking code
-    const hotjarTrackingCode = `(function(h,o,t,j,a,r){
+    const hotjarTrackingCode = (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3606196,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');;
 
     const script = document.createElement("script");
     script.appendChild(document.createTextNode(hotjarTrackingCode));
