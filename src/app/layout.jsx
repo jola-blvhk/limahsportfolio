9de426d1fc080a9ee33fb5720e372f1996670706
jola-blvhk <awujoolabello@gmail.com>
2023-08-10 23:21:05 +0100

    hot jar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 import "./globals.css";
 
 import { Poppins, Comfortaa } from "next/font/google";
-import { Header, Footer, HotjarAnalytics } from "../components/index";
+import { Header, Footer } from "../components/index";
 
 const comfortaa = Comfortaa({ subsets: ["latin"] });
 
@@ -13,13 +13,7 @@ export default function RootLayout({ children }) {
       <body className={`${comfortaa.className}`}>
         <Header />
         {children}
-        <Script
-          id="hotjar"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: <HotjarAnalytics />,
-          }}
-        />
+      \
         <Footer />
       </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,6 +16,7 @@ import reviews from "../components/reviews.json";
 import Link from "next/link";
 import { useEffect } from "react";
 import Head from "next/head";
+import {NextHead}
 
 export default function Home() {
   const scrollToBottom = () => {
@@ -27,16 +28,22 @@ export default function Home() {
 
   useEffect(() => {
     // Replace with your Hotjar tracking code
-    const hotjarTrackingCode = (function(h,o,t,j,a,r){
+    const hotjarTrackingCode = `
+      <!-- Hotjar Tracking Code -->
+      <script>
+       (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3606196,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');;
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      </script>
+      <!-- End Hotjar Tracking Code -->
+    `;
 
-    const script = document.createElement("script");
+    const script = document.createElement('script');
     script.appendChild(document.createTextNode(hotjarTrackingCode));
     document.head.appendChild(script);
   }, []);
