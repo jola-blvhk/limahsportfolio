388da4cfdc8e25d3ebada4065d4d7725fff977eb
jola-blvhk <awujoolabello@gmail.com>
2023-08-12 10:26:23 +0100

    hot jar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,10 +4,15 @@ import "./globals.css";
 
 import { Poppins, Comfortaa } from "next/font/google";
 import { Header, Footer } from "../components/index";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
 const comfortaa = Comfortaa({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    hotjar.initialize(3606196, 6);
+  }, []);
   return (
     <html lang="en">
       <body className={`${comfortaa.className}`}>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,26 +25,6 @@ export default function Home() {
     });
   };
 
-  useEffect(() => {
-    // Replace with your Hotjar tracking code
-    const hotjarTrackingCode = 
-     
-      <script>
-      (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3606196,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-      </script>
-     
-
-    const script = document.createElement("script");
-    script.appendChild(document.createTextNode(hotjarTrackingCode));
-    document.head.appendChild(script);
-  }, []);
   return (
     <main className=" bg-primary-white-100 pt-16">
       <section
