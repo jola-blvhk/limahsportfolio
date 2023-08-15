36751961df7bb7b49f1e19c3ac9a2ad39caf4b30
jola-blvhk <awujoolabello@gmail.com>
2023-08-15 17:17:20 +0100

    carousel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,9 @@
 import Script from "next/script";
 import "./globals.css";
-import Head from "next/head";
+
 
 import { Poppins, Comfortaa } from "next/font/google";
 import { Header, Footer } from "../components/index";
-import { hotjar } from "react-hotjar";
-import { useEffect } from "react";
 
 const comfortaa = Comfortaa({ subsets: ["latin"] });
 
